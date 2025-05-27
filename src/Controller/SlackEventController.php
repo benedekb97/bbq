6b91d4de74a64d3bccc73ad4975eaa9e16f55673
Benedek Burgess <benedekb97@sch.bme.hu>
2025-05-27 11:54:25 +0100

    Handle user verification

---
+++ src/Controller/SlackEventController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SlackEventController extends AbstractController
+{
+    #[Route('slack/event', methods: [Request::METHOD_POST])]
+    public function handle(Request $request): JsonResponse
+    {
+        if ($request->request->get('type') === 'user_verification') {
+            $challenge = $request->request->get('challenge');
+
+            return new JsonResponse([
+                'challenge' => $challenge,
+            ]);
+        }
+
+        return new JsonResponse();
+    }
+}
