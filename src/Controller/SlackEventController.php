938e837b05d18318ef4585e9fd801465e0ddd517
Benedek Burgess <benedekb97@sch.bme.hu>
2025-05-27 12:05:13 +0100

    log request data

--- src/Controller/SlackEventController.php
+++ src/Controller/SlackEventController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,9 +12,15 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SlackEventController extends AbstractController
 {
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {}
+
     #[Route('slack/event', methods: [Request::METHOD_POST])]
     public function handle(Request $request): JsonResponse
     {
+        $this->logger->debug(implode(', ', $request->request->all()));
+
         if ($request->request->get('type') === 'url_verification') {
             $challenge = $request->request->get('challenge');
 
